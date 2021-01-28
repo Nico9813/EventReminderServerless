18d96c84d4954c43a80a8dc5af63159f08ce10ef
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-28 13:00:12 -0300

    lambda post create event

--- src/handlers/createEvent.js
+++ src/handlers/createEvent.js
@@ -1,7 +1,23 @@
+import { v4 as uuid } from 'uuid';
+
 async function createEvent(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1);
+
+  const newEvent = {
+    id: uuid(),
+    title,
+    eventType: 'DAILY',
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+    endingAt: endDate.toISOString()
+  };
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'createEvent from https://codingly.io' }),
+    body: JSON.stringify(newEvent),
   };
 }
 
