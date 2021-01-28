44b464d1b68bdd2e47c8324cc1c5663c968a38ad
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-28 13:14:39 -0300

    lambda get events

--- src/handlers/getEvents.js
+++ src/handlers/getEvents.js
@@ -1,7 +1,22 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function getEvent(event, context) {
+
+  let events;
+
+  try {
+    const result = await dynamodb.scan({ TableName: process.env.EVENTS_TABLE_NAME}).promise();
+    events = result.Items;
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'getEvent from https://codingly.io' }),
+    body: JSON.stringify(events),
   };
 }
 
