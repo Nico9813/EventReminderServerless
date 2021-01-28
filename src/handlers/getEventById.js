341ede2a96907640b68f811d6491666a81630512
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-28 14:18:45 -0300

    lambda get event by id

--- src/handlers/getEventById.js
+++ src/handlers/getEventById.js
@@ -1,7 +1,37 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function getEvent(id) {
+  let event;
+
+  try {
+    const result = await dynamodb.get({
+      TableName: process.env.EVENTS_TABLE_NAME,
+      Key: { id },
+    }).promise();
+
+    event = result.Item;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if (!event) {
+    throw new createError.NotFound(`Event with ID "${id}" not found!`);
+  }
+
+  return event;
+}
+
 async function getEventById(event, context) {
+  const { id } = event.pathParameters;
+  const _event = await getEvent(id);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'getEventById from https://codingly.io' }),
+    body: JSON.stringify(_event),
   };
 }
 
--- src/handlers/getEvents.js
+++ src/handlers/getEvents.js
@@ -4,11 +4,23 @@ import createError from 'http-errors';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getEvent(event, context) {
-
+  const { status } = event.queryStringParameters;
   let events;
 
+  const params = {
+    TableName: process.env.EVENTS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
   try {
-    const result = await dynamodb.scan({ TableName: process.env.EVENTS_TABLE_NAME}).promise();
+    const result = await dynamodb.query(params).promise();
     events = result.Items;
   } catch (error) {
     throw new createError.InternalServerError(error);
