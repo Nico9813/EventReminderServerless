d62ee3cb7d590c780f2068281cd2de7d9cabf6ac
nico9813 <nico.gomez.mbc@gmail.com>
2021-01-29 18:19:18 -0300

    add sqs mail queue to trigger sendMail lambda

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,10 +1,38 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES();
+
+async function sendMail(event, context) {
+  //const record = event.Records[0];
+  //console.log('record processing', record);
+
+  //const email = JSON.parse(record.body);
+  //const { subject, body, recipient } = email;
+
+  const params = {
+    Source: 'nico.gomez.mbc@gmail.com',
+    Destination: {
+      ToAddresses: ["nico.gomez.mbc@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Prueba",
+        },
+      },
+      Subject: {
+        Data: "Prueba mas larga",
+      },
+    },
   };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export const handler = hello;
-
-
+export const handler = sendMail;
